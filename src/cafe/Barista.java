ea647456c55d5937b0aaafbd07a9dfe42747f318
lanxnguyen06 <lanxnguyen06@gmail.com>
2025-11-12 13:48:19 -0500

    fixed it so it's method overloading

--- src/cafe/Barista.java
+++ src/cafe/Barista.java
@@ -11,16 +11,18 @@ import coffee.Coffee;
 public class Barista {
 
     // Single coffee order
-    public static void prepareOrder(Order order, boolean isSingle) {
-        if (order.getCoffees().size() == 1 || isSingle) {
+    public static void prepareOrder(Order order) {
+        if (order.getCoffees().size() == 1) {
             Coffee coffee = order.getCoffees().get(0); // getCoffees returns list of coffees so use arrayList method .get to get the first coffee in the list (even if it's a single order)
             System.out.println("Preparing " + coffee.getSize() + "-oz " + coffee.getType() + " for " + order.getCustomer().getName());
             coffee.make();
             coffee.setReadyForPickup();
             order.markFulfilled();
             System.out.println("Order #" + order.getOrderId() + " ready for pickup!\n");
-        } 
-        else if (order.getCoffees().size() > 1 || !isSingle){ // if more than one coffee or not a single order
+        }
+    }
+     
+    public static void prepareOrder(Order order, boolean isGroupOrder){
             System.out.println("Preparing group order # " + order.getOrderId() + " for " + order.getCustomer().getName());
             for (int i = 0; i < order.getCoffees().size(); i++){
                 Coffee coffee = order.getCoffees().get(i); // iterates through the list of coffees and grabs the one at the i index
@@ -34,7 +36,6 @@ public class Barista {
 
             // prepareOrder(order, true); // delegate to multi-item prepare method
         }
-    }
 
     // TODO: Prepare multiple coffees for a group order
     // Use method overloading and avoid duplicate code!
